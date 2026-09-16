ae5882491d9877f0f7fb344fb772f69b75c3413b
Sai Prasanna Cheedi <153516273+sai-prasann@users.noreply.github.com>
2026-09-16 17:45:26 +0100

    Require admin role for update availability route
    Updated middleware for availability update route to include admin role.

--- NHANES/testwebsite/routes/web.php
+++ NHANES/testwebsite/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
 ->name('dashboard');
 
 Route::patch('/dashboard/{id}/update-availability', [DashboardController::class, 'updateAvailability'])
-->middleware(['auth', 'verified'])
+->middleware(['auth', 'verified', 'role:admin'])
 ->name('update.availability');
 
 Route::post('/dataset/download/{id}', [DashboardController::class, 'downloadAndConvert'])
